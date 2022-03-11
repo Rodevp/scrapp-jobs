c3e4aab105451b63a719bb84539c6a66f1c67dc9
Rodevp <rodriguezrodrigor12@gmail.com>
2022-03-11 14:02:25 -0500

    scrapp inicia session y va hacia jobs

--- app.js
+++ app.js
@@ -5,7 +5,23 @@ const puppeteer = require('puppeteer');
     const page = await browser.newPage();
     await page.goto('https://www.linkedin.com/login/es?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin');
 
-    
+    await page.waitForTimeout(2000)
+    await page.evaluateHandle(() => {
+        const input = document.getElementById('username')
+        const inputPass = document.getElementById('password')
 
+
+        input.value = 'rodriguezrodrigor12@gmail.com'
+        inputPass.value = 'roxd**17'
+
+    })
+
+    const btn = await page.evaluateHandle(() => 
+        document.querySelector('.btn__primary--large')
+    )
+    btn.click()
+
+    await page.waitForTimeout(2000)
+    page.goto('https://www.linkedin.com/jobs/')
   
 })();
