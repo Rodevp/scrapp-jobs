555d42d57893ea1552112f6ba9b2f15091663420
Rodevp <rodriguezrodrigor12@gmail.com>
2022-03-12 08:44:53 -0500

    migrando primera funcionalidad a playwright

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-const puppeteer = require('puppeteer');
+const {chromium} = require('playwright');
 
 (async () => {
-    const browser = await puppeteer.launch({headless: false, defaultViewport: null});
+
+    const browser = await chromium.launch( { headless: false } )
+    const page = await browser.newPage()
+
+    await page.goto('https://www.linkedin.com/login/es?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin')
+
+    await page.waitForTimeout(1000)
+
+    await page.fill('input[id="username"]', 'rodriguezrodrigor12@gmail.com')
+    await page.fill('input[id="password"]', 'roxd**17')
+    await page.click('button[aria-label="Iniciar sesión"]')
+
+   /**
+    * 
+    *  const browser = await puppeteer.launch({headless: false, defaultViewport: null});
     const page = await browser.newPage();
     await page.goto('https://www.linkedin.com/login/es?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin');
 
@@ -27,20 +41,37 @@ const puppeteer = require('puppeteer');
     const inputs_search = [
         {
             tecnology: 'python',
+            countrie: 'Argentina'
+        },
+        {
+            tecnology: 'python',
             countrie: 'Colombia'
-        }
+        },
+        {
+            tecnology: 'python',
+            countrie: 'Chile'
+        },
+        {
+            tecnology: 'python',
+            countrie: 'Ecuador'
+        },
+        {
+            tecnology: 'python',
+            countrie: 'Peru'
+        }        
     ]
 
     for(let i = 0; i < inputs_search.length; i++) {
 
-        await page.waitForTimeout(1500)
-        
         const tecnology = inputs_search[i].tecnology 
         const countrie = inputs_search[i].countrie 
         
-        await page.type('#jobs-search-box-keyword-id-ember31', tecnology)
-        await page.type('#jobs-search-box-location-id-ember31', countrie)
-        await page.click('.jobs-search-box__submit-button')
+        await page.waitForTimeout(5000)
+        const inputs = await page.evaluate(() => document.querySelectorAll('.jobs-search-box__text-input'))
+        console.log(inputs[0], inputs[3])
+
+        //await page.click('.jobs-search-box__submit-button')
+        /**
 
         await page.waitForTimeout(2000)
         const resultTecnology = await page.evaluate( () => {
@@ -49,16 +80,19 @@ const puppeteer = require('puppeteer');
                                  .map( e => e.textContent.replace(/ /g, '').split('\n').join('') )
 
             const textResultsOfTecnology = mapText[1].split('resultados').join('')
+            const tecnology = mapText[0]
+
+            console.log(tecnology)
             
             return textResultsOfTecnology
 
         })
 
-        console.log(resultTecnology)
+        console.log(resultTecnology) */
 
+        //await page.waitForTimeout(2000)
         //await page.goto('https://www.linkedin.com/jobs/')
 
-
-    }
+    //}
 
 })();
